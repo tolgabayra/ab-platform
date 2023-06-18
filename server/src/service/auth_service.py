fb70f16a619d8a205c3e8f756b985c867dd3e65a
Tolga BAYRAK <bayraktolga28@gmail.com>
2023-06-18 22:43:30 +0300

    password

--- server/src/service/auth_service.py
+++ server/src/service/auth_service.py
@@ -1,15 +1,19 @@
 from database import SessionLocal
 from model import User
+from util.jwt import Jwt
 
 db = SessionLocal()
 
 
 class AuthService:
-    
     @staticmethod
     def login(email: str, password: str):
         user = db.query(User).filter(User.email == email).first()
         if user and User.verify_password(password, user.password):
-            return {"message": "Login is successful", "user_id": user.id}
+            access_token = Jwt.generate_access_token(
+                {"id": user.id}, expires_in_minutes=15
+            )
+            refresh_token = Jwt.generate_refresh_token({"id": user.id}, 3)
+            return {"access_token": access_token, "refres_token": refresh_token}
         else:
-            return {"message": "Invalid email or password"}
+            return False
