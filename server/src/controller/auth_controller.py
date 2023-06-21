a513a96c1952fed6840114c6979f38ced29ed37a
Tolga BAYRAK <bayraktolga28@gmail.com>
2023-06-21 09:07:06 +0300

    client

--- server/src/controller/auth_controller.py
+++ server/src/controller/auth_controller.py
@@ -1,11 +1,17 @@
 from fastapi import APIRouter
-from fastapi import Request
 from service.auth_service import AuthService
+from fastapi import Response
 from schema.user_schema import LoginUser, RegisterUser
+from fastapi import HTTPException
 
 router = APIRouter()
+auth_service = AuthService()
 
 
 @router.post("login")
-async def login(user: LoginUser):
-    pass
+async def login(user: LoginUser, response: Response):
+    data = auth_service.login(email=user.email, password=user.password)
+    if data is None:
+        raise HTTPException(status_code=404, detail="Invalid email or password")
+    
+    response.set_cookie(key="access_token": )
