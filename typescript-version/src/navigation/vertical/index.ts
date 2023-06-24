9fa7ecc073d94ae4cb88438a71f89d828de2c0d5
Tolga BAYRAK <bayraktolga28@gmail.com>
2023-06-24 13:23:22 +0300

    newuş

---
+++ typescript-version/src/navigation/vertical/index.ts
@@ -0,0 +1,51 @@
+// ** Icon imports
+import Login from 'mdi-material-ui/Login'
+import HomeOutline from 'mdi-material-ui/HomeOutline'
+import AccountCogOutline from 'mdi-material-ui/AccountCogOutline'
+import AccountPlusOutline from 'mdi-material-ui/AccountPlusOutline'
+import AlertCircleOutline from 'mdi-material-ui/AlertCircleOutline'
+
+// ** Type import
+import { VerticalNavItemsType } from 'src/@core/layouts/types'
+
+const navigation = (): VerticalNavItemsType => {
+  return [
+    {
+      title: 'Dashboard',
+      icon: HomeOutline,
+      path: '/'
+    },
+    {
+      title: 'Account Settings',
+      icon: AccountCogOutline,
+      path: '/account-settings'
+    },
+    {
+      sectionTitle: 'Pages'
+    },
+    {
+      title: 'Login',
+      icon: Login,
+      path: '/pages/login',
+      openInNewTab: true
+    },
+    {
+      title: 'Register',
+      icon: AccountPlusOutline,
+      path: '/pages/register',
+      openInNewTab: true
+    },
+    {
+      title: 'Error',
+      icon: AlertCircleOutline,
+      path: '/pages/error',
+      openInNewTab: true
+    },
+    {
+      sectionTitle: 'User Interface'
+    }
+
+  ]
+}
+
+export default navigation
